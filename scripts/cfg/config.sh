e493838d5fc7e15c5c3abe6d510973338196702e
varunk122 <varunk@iitk.ac.in>
2020-07-22 00:55:40 +0530

    Add commands for the pkg

--- scripts/cfg/config.sh
+++ scripts/cfg/config.sh
@@ -2,5 +2,5 @@
 
 directory_name="ariitk_temp_pkg"                                         #name of your main_directory
 pkg_names=('temp_pkg1' 'temp_pkg2' 'temp_pkg3')                          #name of pkgs you want to create
-node_names=('pkg1' 'pkg2' 'pkg3')          #name of your node corresponding to the particuar pkg
+node_names=('pkg1' 'pkg2' 'pkg3')                                        #name of your node corresponding to the particuar pkg
 dependencies=('depen1 depen2 depen3' 'depen1 depen2' 'depen1')           #name of pkg_xml dependencies space seperated corresponding to particular pkg
---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ ! -f ~/.bash_profiles ]; then
+    touch ~/.bash_profiles    
+fi
+
+if [ ! -d ~/bin ]; then
+    mkdir ~/bin
+fi
+
+echo -e "export PATH=$PATH":$HOME/bin"" >> ~/.bash_profiles
+echo -e "source ~/.bash_profiles" >> ~/.bashrc
+source ~/.bash_profiles
+
+cp -r ./scripts/ariitk_create_metapkg ~/bin/
+cp -r ./scripts/ariitk_create_pkg ~/bin/
+
+chmod +x ~/bin/ariitk_create_metapkg
+chmod +x ~/bin/ariitk_create_pkg
