fb52af66d75b54421247f11941a6c7f042931c1c
varunk122 <varunk@iitk.ac.in>
2020-07-21 18:56:52 +0530

    minor fix

--- scripts/cfg/test.sh
+++ scripts/cfg/test.sh
@@ -5,5 +5,5 @@ source config.sh
 
 echo -e "directory_name = $directory_name"
 echo -e "pkg_names = ${pkg_names[@]}"
-echo -e "node_name = ${node_name[@]}"
+echo -e "node_name = ${node_names[@]}"
 echo -e "dependencies = ${dependencies[@]}"
