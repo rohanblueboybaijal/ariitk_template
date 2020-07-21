bac22bfa6ca9785f39a0f9d4a9d06cf2bb94047c
rohanblueboybaijal <rohanbaijal2702@gmail.com>
2020-07-21 17:17:11 +0530

    Initial commit

---
+++ template_pkg/src/template_pkg_node_name.cpp
@@ -0,0 +1,16 @@
+#include <template_pkg/template_pkg_node_name.hpp>
+
+namespace ariitk::template_pkg_name {
+
+TemplatePkg::templatePkg() {
+}
+
+TemplatePkg::~TemplatePkg() {
+}
+
+TemplatePkg::run() {
+}
+
+TemplatePkg::init() {
+}
+} //namesapce ariitk::template_pkg_name
---
+++ template_pkg/src/template_pkg_node_name_node.cpp
@@ -0,0 +1,22 @@
+#include <template_pkg/template_pkg_node_name.hpp>
+
+using namespace ariitk::template_pkg_name;
+
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "template_pkg_node_name_node");
+    ros::nodeHandle nh;
+
+    TemplatePkg obj;
+
+    obj.init(nh);
+
+    ros::Rate loop_rate(10);
+
+    while(ros::ok()) {
+        ros::spinOnce();
+        obj.run();
+        loop_rate.sleep();
+    }
+
+    return 0;
+} 
