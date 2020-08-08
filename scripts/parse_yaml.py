d7566fe1c9dcf17c7493970b479bbe66cf915843
varunk122 <varunk@iitk.ac.in>
2020-08-09 04:51:33 +0530

    Issue in cd command in ariitk_create_metapkg(yaml)

--- scripts/parse_yaml.py
+++ scripts/parse_yaml.py
@@ -8,16 +8,22 @@ else :
 
 import yaml
 import subprocess
+import sys
 
 if(yaml.__version__ < "5.3.1"):
     os.system("pip3 install -U pyYAML > /dev/null")
 
-with open(r"cfg/config.yaml") as file:
+path = sys.argv[1]
+f = open(path+ "/config.sh","w+")
+f.write("#!usr/bin/env bash\n")
+with open(path + "/config.yaml") as file:
     yaml_file = yaml.load(file ,Loader=yaml.FullLoader)
 
     for item , value in yaml_file.items():
-        command  =  "export" + item + "=(" + \
-                    " ".join(["'" + string.replace("\t\t\t","").replace("'","").replace("[","").replace("]","").strip() + "'" \
+        command  =  item + "=(" + \
+                    " ".join(["'" + string.replace("\t","").replace("'","").replace("[","").replace("]","").strip() + "'" \
                     for string in value.strip().replace("-","").split("\n")]) \
                     + ")"
-        subprocess.Popen(command, shell=True, executable='/bin/bash')
+        # subprocess.Popen(command, shell=True, executable='/bin/bash')
+        f.write(command + "\n")
+        
--- setup.sh
+++ setup.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+config_file=${2}
+
+if [[ ${1} != "--set" ]] && [[ ${1} != "-s" ]]; then
+  printf "error: invalid command\n"
+  exit
+fi
+
+if [[ $config_file != "yaml" ]] && [[ $config_file != "sh" ]]; then
+  printf "error: invalid command\n"
+  exit
+fi
+
 if [ ! -f ~/.bash_profile ]; then
     touch ~/.bash_profile    
 fi
@@ -12,11 +24,13 @@ if [[ ! $(grep -x "export PATH=$PATH":$HOME/bin"" ~/.bash_profile) ]]; then
   echo -e "export PATH=$PATH":$HOME/bin"" >> ~/.bash_profile
 fi
 
+sed -i "/export ariitk_config_file_type=yaml/d" ~/.bash_profile
+sed -i "/export ariitk_config_file_type=sh/d" ~/.bash_profile
+echo -e "export ariitk_config_file_type=${config_file}" >> ~/.bash_profile
+
 if [[ ! $(grep -x "source ~/.bash_profile" ~/.bashrc) ]]; then
   sed -i "1i source ~/.bash_profile" ~/.bashrc
 fi
-
-source ~/.bash_profile
 
 cp -r ./scripts/ariitk_create_metapkg ~/bin/
 cp -r ./scripts/ariitk_create_pkg ~/bin/
